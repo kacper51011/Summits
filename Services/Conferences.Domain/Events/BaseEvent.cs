674ec5975aa0bbc222803a55e070c99d48325945
Kacper <kacper.tylec1999@gmail.com>
2024-05-26 20:03:11 +0200

    BaseEvent

---
+++ Services/Conferences.Domain/Events/BaseEvent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conferences.Domain.Events
+{
+    public abstract class BaseEvent
+    {
+        public string Id { get; set; }
+        public EventType EventType { get; set; }
+        public int Version { get; set; }
+        public DateTime TimeStampUtc { get; set; }
+
+        public BaseEvent(EventType type)
+        {
+            Id = Guid.NewGuid().ToString();
+            EventType = type;
+            TimeStampUtc = DateTime.UtcNow;
+        }
+
+    }
+}
