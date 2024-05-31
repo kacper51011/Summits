a238c6d49dde645f6b144618f37c0d97282ed918
Kacper <kacper.tylec1999@gmail.com>
2024-05-31 13:43:31 +0200

    Initial interfaces for repository and eventStore

---
+++ Services/Conferences.Domain/Interfaces/IConferenceRepository.cs
@@ -0,0 +1,19 @@
+﻿using Conferences.Domain.Aggregates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conferences.Domain.Interfaces
+{
+    public interface IConferenceRepository
+    {
+        public Task<List<Conference>> GetAllConferences();
+        public Task<Conference> GetConferenceById(string conferenceId);
+        public Task CreateOrUpdateConference(Conference conference);
+
+
+        
+    }
+}
---
+++ Services/Conferences.Domain/Interfaces/IEventStore.cs
@@ -0,0 +1,14 @@
+﻿using Conferences.Domain.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conferences.Domain.Interfaces
+{
+    public interface IEventStore
+    {
+        public Task SaveToEventStore(BaseEvent baseEvent);
+    }
+}
