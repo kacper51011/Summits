7270307df3b6f38119f4a6e03ca39ada22e0af9e
Kacper <kacper.tylec1999@gmail.com>
2024-06-05 20:29:47 +0200

    Actualised Errors

---
+++ Services/Conferences.Domain/Errors/Conference/ConferenceTerminationsExceptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conferences.Domain.Errors.Conference
+{
+    public static class ConferenceTerminationsExceptions
+    {
+        public static ErrorType ConferenceAlreadyEnded => new("Can not modify ended conference");
+        public static ErrorType ConferenceAlreadyCanceled => new("Can not modify canceled conference");
+    }
+}
