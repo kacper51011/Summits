96011f196277a0994acf260c94c489973ae6ae28
Kacper <kacper.tylec1999@gmail.com>
2024-06-05 16:06:59 +0200

    skeleton of createconferencecommand without repository and logging

--- Services/Conferences.Application/Commands/Conferences/CreateConferenceCommand.cs
+++ Services/Conferences.Application/Commands/Conferences/CreateConferenceCommand.cs
@@ -1,4 +1,9 @@
 ﻿using Conferences.Application.Dto;
+using Conferences.Application.DtoMappers;
+using Conferences.Domain.Aggregates;
+using Conferences.Domain.Builders;
+using Conferences.Domain.Errors;
+using Conferences.Domain.Utilities;
 using MediatR;
 using System;
 using System.Collections.Generic;
@@ -8,15 +13,30 @@ using System.Threading.Tasks;
 
 namespace Conferences.Application.Commands.Conferences
 {
-    public record CreateConferenceCommand(CreateConferenceDto Dto): IRequest
+    public record CreateConferenceCommand(CreateConferenceDto Dto): IRequest<Result<string>>
     {
     }
 
-    public class CreateConferenceCommandHandler : IRequestHandler<CreateConferenceCommand>
+    public class CreateConferenceCommandHandler : IRequestHandler<CreateConferenceCommand, Result<string>>
     {
-        public async Task Handle(CreateConferenceCommand request, CancellationToken cancellationToken)
+        public async Task<Result<string>> Handle(CreateConferenceCommand request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            Result<Conference> result = ConferenceBuilder.Create()
+                .SetName(request.Dto.Name)
+                .SetDescription(request.Dto.Description)
+                .SetTicketPool(request.Dto.TicketPool.MapToTicketPool())
+                .SetLectures(request.Dto.Lectures.MapToLectures())
+                .SetDates(request.Dto.StartDateUtc, request.Dto.EndDateUtc)
+                .Build();
+
+            if (result.IsSuccess)
+            {
+                return Result.Success(result.Data!.ConferenceId);
+            }
+            else
+            {
+                return Result.Failure<string>(result.Error);
+            }
         }
     }
 }
