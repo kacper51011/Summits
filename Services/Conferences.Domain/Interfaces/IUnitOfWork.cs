17bbe199a5b22d3d37c529038ffa3ac5420f39a6
Kacper <kacper.tylec1999@gmail.com>
2024-06-07 15:58:39 +0200

    IUnit of work interface

---
+++ Services/Conferences.Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conferences.Domain.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        // for changes coming from event store
+        public Task SaveChangesAsync();
+
+        // for changes which will be send to event store
+        public Task SaveChangesAsyncAndNotify();
+
+    }
+}
