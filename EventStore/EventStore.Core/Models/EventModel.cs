fcd688b8cbcf70cc064f04473a4e347ccc1300ed
Kacper <kacper.tylec1999@gmail.com>
2024-06-14 20:23:49 +0200

    Modified models

--- EventStore/EventStore.Core/Models/EventModel.cs
+++ EventStore/EventStore.Core/Models/EventModel.cs
@@ -1,21 +1,25 @@
-﻿using MongoDB.Bson.Serialization.Attributes;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using EventStore.Core.Attributes;
+using EventStore.Core.Events.Conferences;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
 
 namespace EventStore.Core.Models
 {
+
+    [BsonDiscriminator(RootClass = true, Required = true)]
+    [BsonKnownTypes(typeof(ConferenceEnded))]
+    [JsonPolymorphic(UnknownDerivedTypeHandling = JsonUnknownDerivedTypeHandling.FailSerialization)]
+    [JsonDerivedType(typeof(ConferenceEnded))]
+    //[ConferenceEventsJsonParent]
     public class EventModel
     {
         [BsonId]
         [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
-        public string Id { get; set; }
+
+        public string? Id { get; set; }
         public string AggregateId { get; set; }
         public int Version { get; set; }
         public string EventType { get; set; }
         public DateTime TimeStampUtc { get; set; }
-        public BaseEvent EventData { get; set; }
     }
 }
